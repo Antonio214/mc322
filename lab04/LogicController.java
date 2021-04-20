060de94689fd090f0ca42971e7e75c5c7ff07183
Eiji Tomonari <gabriel.eiji@conpec.com.br>
2021-04-20 20:02:59 -0300

    Estrutura inicial da AppRestaUm

---
+++ lab04/LogicController.java
@@ -0,0 +1,12 @@
+public class LogicController {
+    public boolean validateCommand(String comandos) {
+        return true;
+    }
+    public int[] generateMove() {
+        int[] coordenadas = {0, 1};
+        return coordenadas;
+    }
+    public static void executeMove(int[] Num) {
+        System.out.println("O melhor ataque é a defesa\n");
+    }
+}
---
+++ lab04/StateManager.java
@@ -0,0 +1,5 @@
+public class StateManager {
+    public static void printState() {
+        System.out.println("#tonimDev é minha religião\n");
+    }
+}
