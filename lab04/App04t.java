b72b29221f61260a1b8b5031d6993a1867bb9482
Antonio Junior ra165677 <fXXXXXX@dac.unicamp.br>
2021-04-21 11:48:52 -0300

    fix converter

--- lab04/App04t.java
+++ lab04/App04t.java
@@ -1,6 +1,6 @@
 public class App04t {
     public static void main(String[] args) {
-        String str = "f4";
+        String str = "a7";
 
         int pos = Converter.parseStringPos(str);
         System.out.println(pos);
--- lab04/Converter.java
+++ lab04/Converter.java
@@ -1,35 +1,33 @@
 public class Converter {
 
     /*
-    *   Recebe uma string no formato f4
-    *   Retorna a posição na string Base
-    *   Ex: f4 -> 29
-    */
+     * Recebe uma string no formato f4 Retorna a posição na string Base Ex: f4 -> 29
+     */
     public static int parseStringPos(String stringPos) {
         char letter = stringPos.charAt(0);
         char num = stringPos.charAt(1);
 
         int line = turnNumToLine(num);
         int col = turnLetterToCol(letter);
-               
+
         int pos = line * 8 + col;
         return pos;
     }
 
-    static private int turnNumToLine(char num){
+    static private int turnNumToLine(char num) {
         final int offset = 7;
-        
+
         int value = Character.getNumericValue(num);
         int line = (value - offset) * -1;
-        
-        return 1;
+
+        return line;
     }
 
-    static private int turnLetterToCol(char letter){
+    static private int turnLetterToCol(char letter) {
         final int offset = 97;
 
         int value = letter;
-        int col = letter - offset;
+        int col = value - offset;
 
         return col;
     }
