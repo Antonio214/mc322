e0b09a816c9a23843b8d9d3868c4a9a4f52c8470
Gabriel Tomonari <gabrieltomonari@gmail.com>
2021-04-21 10:02:28 -0300

    Alterações no CSV Reader

--- lab04/AppRestaUm.java
+++ lab04/AppRestaUm.java
@@ -1,10 +1,16 @@
 public class AppRestaUm {
     public static void executaJogo(String caminho) {
+        
+        // CSV Reader
         CSVReader csv = new CSVReader();
-        LogicController logicController = new LogicController();
-        // StateManager stateManager = new StateManager();
         csv.setDataSource(caminho);
         String commands[] = csv.requestCommands();
+        
+        // Instances
+        LogicController logicController = new LogicController();
+        // StateManager stateManager = new StateManager();
+
+        // Main logic
         for (int i = 0; i < commands.length; i++) {
             logicController.validateCommand(commands[i]);
             int Move[] = logicController.generateMove();
